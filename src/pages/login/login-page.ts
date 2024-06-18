7aa63204ec5c953abd5857a0a28fb7ec4d1ec81c
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-18 15:45:49 +0100

    Trabajando en css de  login.ts

--- src/pages/login/login-page.ts
+++ src/pages/login/login-page.ts
@@ -8,24 +8,37 @@ export class LoginPage extends LitElement {
 
   static styles = css`
     :host {
-
-    
+    padding: 1.5rem;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    }
+     & .form-container {
+        display: grid;
+        place-items: center;
+        height: 100%;
+        width: 100%;
+        max-width: 80%
     }
     form {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 1rem;
-    }
+        display: grid;
+        gap: 1rem;
+        width: 100%;
+        }
   `;
   render () {
     return html`
       <h1>Login Page</h1>
-      <form>
-        <md-outlined-text-field label="username" required ></md-outlined-text-field>
-         <md-outlined-text-field label="password" required></md-outlined-text-field>
-         <md-filled-button>Login</md-filled-button>
-      </form>
+      <div class="form-container">
+        <form>
+          <md-outlined-text-field supporting-text="*requerid" type="text"  label="Username"     requerid minLength="3" ></md-outlined-text-field>
+           <md-outlined-text-field supporting-text="*requerid" type="text" iconTrailing="visibility" label="Password"  minLength="8" required>
+            <md-icon slot="trailing-icon">visibility</md-icon>
+          </md-outlined-text-field>
+           <md-filled-button>Login</md-filled-button>
+        </form>
+      </div>
 
     `;
   }
