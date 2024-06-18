3117360019800a528e29dd9b129c13ff7efd78a3
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-18 13:01:35 +0100

    header y footer terminado

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -2,6 +2,7 @@ import { LitElement,html,css } from "lit";
 import { customElement } from "lit/decorators.js";
 
 
+
 @customElement("footer-component")
 export class FooterComponent extends LitElement {
   static styles = css`
--- src/components/header.ts
+++ src/components/header.ts
@@ -1,5 +1,6 @@
 import { LitElement,html,css } from "lit";
 import { customElement } from "lit/decorators.js";
+import "@material/web/icon/icon.js"
 
 @customElement("header-component")
 export class HeaderComponent extends LitElement {
@@ -8,20 +9,30 @@ export class HeaderComponent extends LitElement {
     header {
       display: grid;
       grid-template-columns: repeat(3, 1fr);
+      padding: 0.5rem 1rem;
 
        & h1 {
        text-align: center;
        font-size: 1rem;
+       margin: 0;
+       place-self: center;
        }
+
+        & .icon-arrow {
+        place-self: center start;
+        }
+        & .icon-language {
+        place-self: center end;
+        }
      
       }
   `;
   render () {
     return html`
       <header>
-        <h1>HeaderComponent</h1>
-        <h1>Toker</h1>
-        <h1>HeaderComponent</h1>
+       <md-icon class="icon-arrow">arrow_back</md-icon>
+        <h1>Tasker App</h1>
+       <md-icon class="icon-language">language</md-icon>
       </header>
     `;
   }
