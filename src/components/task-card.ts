c61517888756b641d0f98f63f80c2345e222382f
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-19 13:49:50 +0100

    Trabajando en las tarjetas de  task-card.ts

--- src/components/task-card.ts
+++ src/components/task-card.ts
@@ -10,10 +10,52 @@ export class TaskCard extends LitElement {
   static styles = css`
     .task-card {
       border: 1px solid #ccc;
+      background-color: #f9f9f9;
       border-radius: 5px;
-      padding: 10px;
-      margin: 10px;
-    }
+      padding: 0.8rem;
+      margin: 0.5rem 1rem;
+        & :first-child {
+          margin-top: 0;
+        }
+          &  h2 {
+          font-size: 1rem;
+          font-weight: 900;
+          }
+          }
+    .ctrl-container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-top: 1px solid #ccc;
+      padding-top: 0.6rem;
+
+        & .ctrl-task {
+          display: flex;
+          gap: 0.6rem;
+          
+          & span {
+            --md-icon-size: 0.75rem;
+            border: 1px solid #ccc;
+            padding: 0.25rem 0.5rem;
+            border-radius: 5px;
+            font-size: 0.75rem;
+            display: flex;
+            align-items: center;
+            gap: 0.5rem;
+            }
+            & md-icon {
+              cursor: pointer;
+              }
+          }
+      }
+    .ctrl-btn {
+      display: flex;
+      gap: 0.8rem;
+
+        & md-icon {
+          cursor: pointer;
+        }
+      }
   `;
 
   render () {
@@ -21,16 +63,14 @@ export class TaskCard extends LitElement {
       <div class="task-card">
         <h2>${this.task.title}</h2>
         <p>${this.task.description}</p>
-        <div>
-          <div>
-            ${this.task.tags.map((tag: any) => html`<span>${tag}</span>`)}
+        <div class="ctrl-container">
+          <div class="ctrl-task">
+            ${this.task.tags.map((tag: any) => html`<span>${tag} <md-icon>close</md-icon></span>`)}
           </div>
-          <div>
-            <button @click=${() => this.dispatchEvent(new CustomEvent('edit-task', { detail: this.task }))}>Edit</button>
-            <button @click=${() => this.dispatchEvent(new CustomEvent('delete-task', { detail: this.task }))}>Delete</button>
-
+          <div class="ctrl-btn">
+            <md-icon @click=${() => this.dispatchEvent(new CustomEvent('delete-task', { detail: this.task }))}>delete</md-icon>
+            <md-icon @click=${() => this.dispatchEvent(new CustomEvent('edit-task', { detail: this.task }))}>edit</md-icon>
           </div>
-
         </div>
       </div>
     `;
