b4b20d9f6514e6bbccdeeaf78965594884adfdc3
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-19 15:06:36 +0100

    Cambiado el idioma en footer.ts

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -1,10 +1,16 @@
 import { LitElement,html,css } from "lit";
 import { customElement } from "lit/decorators.js";
+import {t, updateWhenLocaleResourcesChange} from '@open-cells/localize';
 
 
 
 @customElement("footer-component")
 export class FooterComponent extends LitElement {
+  constructor() {
+    super();
+    // @ts-ignore
+    updateWhenLocaleResourcesChange(this);
+  }
   static styles = css`
     footer {
       text-align: center;
@@ -19,7 +25,7 @@ export class FooterComponent extends LitElement {
     
     return html`
       <footer>
-        <p>&copy; ${new Date().getFullYear()} by Bubulazi</p>
+        <p>&copy; ${new Date().getFullYear()} ${t('footer-by')} Bubulazi</p>
       </footer>
     `;
   }
