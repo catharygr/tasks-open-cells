c47252a8e94c3b0ae58958756647ba44ad17dde4
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-19 17:13:26 +0100

    Trabajando en el css del componente not-found-page.ts

--- src/pages/not-found/not-found-page.ts
+++ src/pages/not-found/not-found-page.ts
@@ -1,10 +1,20 @@
 import { LitElement,html,css } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, state } from "lit/decorators.js";
 import '@material/web/button/filled-button.js';
+import {t, updateWhenLocaleResourcesChange} from '@open-cells/localize';
 
 
 @customElement("not-found-page")
 export class NotFound extends LitElement {
+  constructor() {
+    super();
+    // @ts-ignore
+    updateWhenLocaleResourcesChange(this);
+  }
+
+  @state()
+  private _lenguage = '';
+  private _alt = 'en';
 
 
   static styles = css `
@@ -26,13 +36,21 @@ export class NotFound extends LitElement {
             margin-top: 2rem;
           }
   `;
+
+
   render () {
+    const imageName = this._lenguage === 'es' ? 'not-found-es.png' : 'not-found-en.png';
+    const altName = this._alt === 'es' ? 'Imágenes de droides' : 'Images of droids';
     return html`
     <div class="container-no-found">
-      <h2>Lo sentimos, no hemos encontrado esa página, puedes volver a la home: </h2>
+      <h2>${t('not-title')}</h2>
       <md-filled-button class="btn-not-found" @click=${() => window.location.href = '/'}>HOME</md-filled-button>
-      <img src="/images/not-found.png" alt="Imagen de error"/>
+      <img src="/images/${imageName}" alt=${altName}/>
     </div>
     `;
   }
 }
+
+
+// Estos no son los droides que estás buscando.
+// We're sorry, we couldn't find that page. You can go back to the home page:
