59e3e0d06c3f702afe602bc5ff0f9f1e386146e6
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-19 15:15:38 +0100

    Trabajando en el componente not-found-page.ts

--- src/pages/not-found/not-found-page.ts
+++ src/pages/not-found/not-found-page.ts
@@ -1,12 +1,15 @@
 import { LitElement,html } from "lit";
 import { customElement } from "lit/decorators.js";
+import { PageController } from "@open-cells/page-controller";
 
 @customElement("not-found-page")
 export class NotFound extends LitElement {
+  pageController = new PageController(this);
   render () {
     return html`
-      <h1>404 Page</h1>
-      <p>This is the 404 page</p>
+      <h2>Lo sentimos,no hemos encontrado esa página, puedes volver a la home: </h2>
+     <button @click=${() => this.pageController.navigate("home")}>Home</button>
+      img
     `;
   }
 }
