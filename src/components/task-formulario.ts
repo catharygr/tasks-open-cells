36f072c44cdafe66bea484ee297ffc182e080cde
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-20 13:04:13 +0100

    Configuracion del prettier

--- src/components/task-formulario.ts
+++ src/components/task-formulario.ts
@@ -1,11 +1,11 @@
-import { LitElement,html,css } from "lit";
-import { customElement } from "lit/decorators.js";
+import { LitElement, html, css } from 'lit';
+import { customElement } from 'lit/decorators.js';
 import '@material/web/textfield/outlined-text-field.js';
 import '@material/web/select/outlined-select.js';
 import '@material/web/select/select-option.js';
 import '@material/web/button/filled-button.js';
 
-@customElement("task-formulario")
+@customElement('task-formulario')
 export class TaskFormulario extends LitElement {
   static styles = css`
     :host {
@@ -13,22 +13,30 @@ export class TaskFormulario extends LitElement {
       flex-direction: column;
       gap: 0.5rem;
       padding: 0 1rem;
-        & last-child {
-          margin-top: 1rem;
-          }
+      & last-child {
+        margin-top: 1rem;
+      }
     }
   `;
-  render () {
+  render() {
     return html`
-
       <md-outlined-select label="Select a type">
         <md-select-option value="personal">Personal</md-select-option>
         <md-select-option value="work">Work</md-select-option>
         <md-select-option value="shopping">Shopping</md-select-option>
       </md-outlined-select>
-      <md-outlined-text-field type="text" label="Title" required></md-outlined-text-field>
-      <md-outlined-text-field type="textarea" label="Description" required rows="6"></md-outlined-text-field>
-      <md-outlined-text-field type="text" label="Tags" ></md-outlined-text-field>
+      <md-outlined-text-field
+        type="text"
+        label="Title"
+        required
+      ></md-outlined-text-field>
+      <md-outlined-text-field
+        type="textarea"
+        label="Description"
+        required
+        rows="6"
+      ></md-outlined-text-field>
+      <md-outlined-text-field type="text" label="Tags"></md-outlined-text-field>
       <md-filled-button>Create</md-filled-button>
     `;
   }
