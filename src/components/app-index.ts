712183cdd5c3820b9c90b5df29b369898520eb9a
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-22 11:57:19 +0100

    Trabajando en retroceso a la página correspodientes

--- src/components/app-index.ts
+++ src/components/app-index.ts
@@ -4,33 +4,34 @@ import { customElement } from 'lit/decorators.js';
 import { ElementController } from '@open-cells/element-controller';
 import { routes } from '../router/routes.js';
 import { styles } from './app-index.css.js';
-import "./header.ts"
-import "./footer.ts"
+import './header.ts';
+import './footer.ts';
 
 startApp({
   routes,
   mainNode: 'app-content',
-  persistentPages: ['home'], 
+  viewLimit: 1,
   // @ts-ignore
-  interceptor: function(navigation, ctx) {
-    let intercept = false
+  interceptor: function (navigation, ctx) {
+    let intercept = false;
     let redirect;
 
-    if(
-      !sessionStorage.getItem("userToken") && 
+    if (
+      !sessionStorage.getItem('userToken') &&
       navigation.to?.page !== 'login'
     ) {
-      intercept = true; 
-      redirect = {page: 'login', params: {}}
+      intercept = true;
+      redirect = { page: 'login', params: {} };
     }
-    if(sessionStorage.getItem("userToken") && navigation.to?.page === 'login') {
-      intercept = true; 
+    if (
+      sessionStorage.getItem('userToken') &&
+      navigation.to?.page === 'login'
+    ) {
+      intercept = true;
     }
-    return {intercept, redirect};
-  }
+    return { intercept, redirect };
+  },
 });
-
-
 
 @customElement('app-index')
 export class AppIndex extends LitElement {
@@ -45,7 +46,6 @@ export class AppIndex extends LitElement {
         <slot></slot>
       </main>
       <footer-component></footer-component>
-
     `;
   }
 }
--- src/components/header.ts
+++ src/components/header.ts
@@ -1,10 +1,15 @@
-import { LitElement,html,css } from "lit";
-import { customElement } from "lit/decorators.js";
-import "@material/web/icon/icon.js"
-import {t, updateWhenLocaleResourcesChange, setLang} from '@open-cells/localize';
+import { LitElement, html, css } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import '@material/web/icon/icon.js';
+import {
+  t,
+  updateWhenLocaleResourcesChange,
+  setLang,
+} from '@open-cells/localize';
 
-@customElement("header-component")
+@customElement('header-component')
 export class HeaderComponent extends LitElement {
+  [x: string]: any;
   constructor() {
     super();
     // @ts-ignore
@@ -17,34 +22,40 @@ export class HeaderComponent extends LitElement {
       grid-template-columns: repeat(3, 1fr);
       padding: 0.5rem 1rem;
 
-       & h1 {
-       text-align: center;
-       font-size: 1rem;
-       margin: 0;
-       place-self: center;
-       }
+      & h1 {
+        text-align: center;
+        font-size: 1rem;
+        margin: 0;
+        place-self: center;
+      }
 
-        & .icon-arrow {
+      & .icon-arrow {
         place-self: center start;
-        }
-        & .icon-language {
+      }
+      & .icon-language {
         place-self: center end;
         cursor: pointer;
-        }
-     
       }
+    }
   `;
 
+  static inbounds = {
+    currentPage: { channel: '__oc_app' },
+  };
+
   toggleLanguage() {
-    setLang(document.documentElement.lang === 'en' ? 'es' : 'en')
+    setLang(document.documentElement.lang === 'en' ? 'es' : 'en');
   }
 
-  render () {
+  render() {
+    console.log(this.currentPage?.value.currentPage);
     return html`
       <header>
-       <md-icon class="icon-arrow">arrow_back</md-icon>
-        <h1>${t('app-title') ?? "Taks App"}</h1>
-       <md-icon @click=${this.toggleLanguage} class="icon-language">language</md-icon>
+        <md-icon class="icon-arrow">arrow_back</md-icon>
+        <h1>${t('app-title') ?? 'Taks App'}</h1>
+        <md-icon @click=${this.toggleLanguage} class="icon-language"
+          >language</md-icon
+        >
       </header>
     `;
   }
