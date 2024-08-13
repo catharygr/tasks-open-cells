8e5bf92317f16757fe78b22417540b06e63a3bad
catharygr <103951447+catharygr@users.noreply.github.com>
2024-08-13 14:59:19 +0100

    Refactoración del código en header.ts

--- src/components/header.ts
+++ src/components/header.ts
@@ -53,15 +53,7 @@ export class HeaderComponent extends LitElement {
   }
 
   render() {
-    return html`
-      <header>
-        ${this.displayIcon()}
-        <h1>${t('app-title') ?? 'Taks App'}</h1>
-        <md-icon @click=${this.toggleLanguage} class="icon-language"
-          >language</md-icon
-        >
-      </header>
-    `;
+    return html``;
   }
 
   displayIcon() {
